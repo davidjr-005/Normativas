1b24ddba6b37ce60b487d1a919d42e7c36ccc05a
davidjr-005 <jimenezruizdavid2@gmail.com>
2026-04-28 21:40:43 +0200

    Add authentication middleware to getPets route

--- Despliegue _SG_Aplicacion_Web/backend/src/routes/adoptions.routes.js
+++ Despliegue _SG_Aplicacion_Web/backend/src/routes/adoptions.routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const ctrl = require('../controllers/adoptions.controller');
 const auth = require('../middleware/auth');
 
-router.get('/pets', ctrl.getPets);
+router.get('/pets', auth, ctrl.getPets);
 router.get('/', auth, ctrl.getAll);
 router.post('/', auth, ctrl.adopt);
 
